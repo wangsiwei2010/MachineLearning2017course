6f8f318677425a423c2a61c6873a8d22c0d62d4f
wangsiwei2010 <1551976427@qq.com>
2017-11-18 09:51:12 +0800

    完成svm的matlabQP实现

--- QPsvm.m
+++ QPsvm.m
@@ -2,7 +2,7 @@ function [ w ] = QPsvm( x,y )
 %QPSVM 此处显示有关此函数的摘要
 %   此处显示详细说明
 [m, n] = size(x);
-x = [x, ones(m, 1)];
+x = [x,ones(m, 1)];
 
 H = zeros(n+1, n+1);
 H(1:n, 1:n) = eye(n);
--- svmQP_demo.m
+++ svmQP_demo.m
@@ -4,16 +4,15 @@ clear all
 close all
 
 %% generate data
-nsamples = 200;
-% training data
-[x, y] = tcdataGenerator(nsamples);
-% testing data
-[xt, yt] = tcdataGenerator(nsamples);
-[m n] = size(x);
 
-
+iris_data = load('iris_dataset.mat')
+x = iris_data.irisInputs'
+x = x(:,1:2)
+y = iris_data.irisTargets'
+y = y(:,1)
+y(y==0) = -1;
 %% Quadratic Programming Solver
-w = tcsvmQP(x, y);
+w = QPsvm(x, y);
 
 %% Visualize Results
 xmin = min(x(:))-1;
@@ -21,9 +20,9 @@ xmax = max(x(:))+1;
 data_pos = x(find(y==1),:);
 data_neg = x(find(y==-1),:);
 
-scatter(data_pos(:, 1), data_pos(:, 2), 'b+', 'SizeData', 200, 'LineWidth', 2);
+scatter(data_pos(:, 1), data_pos(:, 2), 'b*', 'SizeData', 2, 'LineWidth', 0.1);
 hold on
-scatter(data_neg(:, 1), data_neg(:, 2), 'gx', 'SizeData', 200, 'LineWidth', 2);
+scatter(data_neg(:, 1), data_neg(:, 2), 'gx', 'SizeData', 2, 'LineWidth', 0.1);
 axis tight
 
 margin = xmin:0.1:xmax;
